65a0054a67c19c479f34bb8e6f970c115cc6de8d
Danil <danya.shaxvorostov@gmail.com>
2023-05-09 16:16:52 +0300

    1FilterStrings

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿static List<string> FilterStrings(string[] array)
+{
+List<string> filteredArray = new List<string>();
+foreach (string str in array)
+{
+if (str.Length <= 3)
+{
+filteredArray.Add(str);
+}
+}
+return filteredArray;
+}
