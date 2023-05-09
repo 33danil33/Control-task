ecf90ddcd786d98415b71ddde2786f9afd16cb74
Danil <danya.shaxvorostov@gmail.com>
2023-05-09 17:26:17 +0300

    4

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿static List<string> FilterStrings(string[] array)
+﻿class Program
+{
+static List<string> FilterStrings(string[] array)
 {
 List<string> filteredArray = new List<string>();
 foreach (string str in array)
@@ -9,4 +11,18 @@ filteredArray.Add(str);
 }
 }
 return filteredArray;
+}
+static void Main()
+{
+Console.WriteLine("Введите элементы массива, разделенные пробелами:");
+string input = Console.ReadLine()!;
+string[] array = input.Split();
+
+List<string> filteredArray = FilterStrings(array);
+Console.WriteLine("Результат:");
+foreach (string str in filteredArray)
+{
+Console.WriteLine(str);
+}
+}
 }
